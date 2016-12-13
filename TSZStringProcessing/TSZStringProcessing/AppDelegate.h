9fb0efd7f09980e903aea36739f7ce68b53f6a8d
tang <tangdream@163.com>
2016-12-13 14:57:01 +0800

    init

---
+++ TSZStringProcessing/TSZStringProcessing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TSZStringProcessing
+//
+//  Created by tang on 16/12/13.
+//  Copyright © 2016年 TangFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TSZStringProcessing/TSZStringProcessing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TSZStringProcessing
+//
+//  Created by tang on 16/12/13.
+//  Copyright © 2016年 TangFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
